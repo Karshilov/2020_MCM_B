5424ccf6de519728a9b3f06ef10fe072c4f15f41
Karshilov <karshilov@foxmail.com>
2020-03-10 01:06:08 +0800

    提交前的最后一稿

--- circular_column.py
+++ circular_column.py
@@ -2,14 +2,17 @@ from matplotlib import pyplot as plt
 import numpy as np
 import random as rnd
 
-SAND_R = 0.00035 * 4
+SAND_R = 0.00135 * 4
 A_LENGTH = 50
 B_LENGTH = 50
 H_HEIGHT = 64
 DECREASE_RATE = 0.899
 EXPEND_RATE = 0.988
 TOTAL = 0.
-WAVE_HEIGHT = 10
+WAVE_HEIGHT = 9
+RAIN_PER_ROUND = 500
+AFFECT_RATIO = 0.8
+ACCUMULATE_LIMITATION = 625
 
 model = np.ndarray([A_LENGTH, B_LENGTH, H_HEIGHT], bool)
 
@@ -108,8 +111,10 @@ def getForce(i, j, k, ftype):
             ret = ret + BASE_F3 * (500. + (float)(H_HEIGHT - k)) / 500.
     return ret
 
+rainAffected = 0
 def recheck():
     counter = 0.
+    global rainAffected
     header = np.ndarray([A_LENGTH, H_HEIGHT], int)
     deleter = np.ndarray([A_LENGTH, B_LENGTH], int)
     for i in range(A_LENGTH):
@@ -122,16 +127,26 @@ def recheck():
     for i in range(A_LENGTH):
         for j in range(B_LENGTH):
             for k in range(H_HEIGHT):
-                if getForce(i, j, k, 0) < 2 * (B_LENGTH - j) / B_LENGTH + 0.8 * round(pow(EXPEND_RATE, header[i, k]), 3):
+                if getForce(i, j, k, 0) < 2.99124 * (B_LENGTH - j) / B_LENGTH + 0.8 * round(pow(EXPEND_RATE, header[i, k]), 3) + 0.0005 * rainAffected * pow(EXPEND_RATE, H_HEIGHT - k):
                     #print (getForce(i, j, k, 0))
                     if legal(i, j, k) and k <= WAVE_HEIGHT + 5:
                             model[i, j, k] = True
                             deleter[i, j] = deleter[i, j] + 1
                 else :
                     #if j == 49 or i == 0 or i == 49 or modelcpy[i, j + 1, k] == True or modelcpy[i + 1, j, k] == True or modelcpy[i - 1, j, k] == True:
-                    if legal(i, j, k) and getForce(i, j, k, 1) < 0.4 * round(pow(DECREASE_RATE, j - header[i, k] + 1), 3):
+                    if legal(i, j, k) and getForce(i, j, k, 1) < 2.99124 * round(pow(EXPEND_RATE, header[i, k]), 3) * round(pow(DECREASE_RATE, j - header[i, k] + 1), 3):
                         model[i, j, k] = True
                         deleter[i, j] = deleter[i, j] + 1
+    for i in range(RAIN_PER_ROUND):
+        x = rnd.randint(0, A_LENGTH - 1)
+        y = rnd.randint(0, B_LENGTH - 1)
+        for z in range(H_HEIGHT):
+            if model[x, y, z] == True and z > 0:
+                rainforce = 0.004 / 0.1 + rnd.uniform(-0.02, 0.02)
+                rainAffected = rainAffected + 1
+                if legal(x, y, z - 1) and (getForce(x, y, z - 1, 0)) / 24 < rainforce:
+                    print ("affected!!!")
+                    model[x, y, z - 1] = True
     for i in range(A_LENGTH):
         for j in range(B_LENGTH):
             kcounter = 0
@@ -158,7 +173,7 @@ ratio = recheck() / TOTAL
 i = 1
 ploty = [1, ratio]
 plotx = [0, i]
-while ratio > 0.75 :
+while ratio > 0.7:
     i = i + 1
     ratio = recheck() / TOTAL
     plotx.append(i)
@@ -185,6 +200,7 @@ np.savetxt("ccnz.txt", outz)
 plt.figure()
 plt.plot(plotx, ploty)
 plt.show()
+# 17
 # 50 50 64 6 times
 # 25 25 32 3 times
 # 75 75 96 26 times
@@ -195,3 +211,5 @@ plt.show()
 # 40 40 100 13 times
 # 80 80 25 3 times
 # 20 20 400 35 times
+
+# 6 times
